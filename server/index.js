2463e22a8942a11de7f4b8a949641f4164ca3e2e
Drew Minns <drew.minns@me.com>
2018-07-22 16:49:09 -0400

    Updated a few things

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const PORT = process.env.CLIENT_PORT || 5000;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(PORT, () =>
-  console.log(`Client running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`)
 );
